f65c40d09d13e0e7af62e8e8d393830998cdbc4a
Chandrasekhar Reddy Muthyala <31596312+cmuth001@users.noreply.github.com>
2017-10-03 15:49:47 -0400

    Update milestone1dump.sql

--- milestone1dump.sql
+++ milestone1dump.sql
@@ -16,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE web-programming_db;
+CREATE DATABASE web_programming_db;
 
 use web_programming_db;
 --
