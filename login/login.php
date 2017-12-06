8cb8c771054b307639a7727a83d415f853c1cd31
npabb001 <npabb001@odu.edu>
2017-12-06 17:25:06 -0500

    git hub login login button in login page

--- login/login.php
+++ login/login.php
@@ -10,7 +10,7 @@
 		<title>Login Page</title>
 		<link rel="stylesheet" type="text/css" href="login.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 	    <script type='text/javascript'>
 			function reCaptchad(){
@@ -33,16 +33,20 @@
 					<label class = "label1" >Password</label>
 					<input class="inputfield" type="password" name="password" />
 					<br/>
-
+					<!-- <input type="checkbox" checked="checked"> Remember me -->
 					<div class="g-recaptcha" data-sitekey="6Le0vAgUAAAAAH_ZWM8tw3It6jkrqLHkFFTMOW-J" data-callback="reCaptchad"></div>
       				<br/>
-					<input class = "btn1" id="myButton" type="submit" value="login" disabled />
-					<input type="checkbox" checked="checked"> Remember me
+					<div><input class = "btn1" id="myButton" type="submit" value="login" disabled /></div>
 					
+					<div class = 'gitBtn'>
+						<button type="button" class="btn btn-primary"><i class="fa fa-github" aria-hidden="true"></i>GITHUB Login </button>
+					</div>
 				</form>
+
 				<div class = "newAccount">
 					<a href= "../signup/signUpPage.php">Create New Account</a>
 				</div>
+
 			</div>
 	</div>
 	</body>
